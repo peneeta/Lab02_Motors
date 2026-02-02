6d11b099688371e8c65187eaf59114d8c3ccda59
Peneeta Wojcik <94078632+peneeta@users.noreply.github.com>
2026-02-02 12:22:40 -0500

    updates to gui

--- lab02_motor_gui.py
+++ lab02_motor_gui.py
@@ -7,7 +7,7 @@ import sys
 
 # TODO Update for motors lab
 
-arduino = serial.Serial(port="/dev/cu.usbmodem1101", baudrate=9600, timeout=0.05)
+# arduino = serial.Serial(port="/dev/cu.usbmodem1101", baudrate=9600, timeout=0.05)
 
 # design the sidebar to pick sensors
 class Sidebar(QWidget):
@@ -34,15 +34,14 @@ class Sidebar(QWidget):
         self.setLayout(layout)
         self.setFixedWidth(180) # fixed width sidebar
 
-# process sensor 1
-class Sensor1Page(QWidget):
+class RCMotorControls(QWidget):
     def __init__(self):
         super().__init__()
         
         layout = QVBoxLayout()
         
         # Page title
-        title = QLabel("Distance")
+        title = QLabel("RC Motor Controls")
         title.setAlignment(Qt.AlignmentFlag.AlignLeft)
         title.setStyleSheet("font-size: 20px;")
         
@@ -51,83 +50,59 @@ class Sensor1Page(QWidget):
         graph_placeholder.setAlignment(Qt.AlignmentFlag.AlignLeft)
         graph_placeholder.setStyleSheet("border: 2px dashed gray; background-color: #ffe6e6;")
         graph_placeholder.setMinimumSize(400, 250)
-
-        self.value_label = QLabel("Latest Distance: --")
-        self.value_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
         
         layout.addWidget(title)
-        layout.addWidget(self.value_label)
         layout.addWidget(graph_placeholder)
         layout.addStretch()
         
         self.setLayout(layout)
-
-    def update_value(self, value):
-        self.value_label.setText(f"Latest Distance: {value} cm")
-
-# process sensor 2
-class Sensor2Page(QWidget):
+        
+class DCMotorControls(QWidget):
     def __init__(self):
         super().__init__()
         
         layout = QVBoxLayout()
         
         # Page title
-        title = QLabel("Temperature + Humidity")
+        title = QLabel("DC Motor Controls")
         title.setAlignment(Qt.AlignmentFlag.AlignLeft)
         title.setStyleSheet("font-size: 20px;")
         
         # Graph placeholder
-        graph_placeholder = QLabel("fjkdsljfkdlajfkdls")
+        graph_placeholder = QLabel("fhdsjkfhjdskhfjdsk")
         graph_placeholder.setAlignment(Qt.AlignmentFlag.AlignLeft)
         graph_placeholder.setStyleSheet("border: 2px dashed gray; background-color: #ffe6e6;")
         graph_placeholder.setMinimumSize(400, 250)
-
-        self.value_label = QLabel("Latest Temperature: --\nLatest Humidity: --")
-        self.value_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
         
         layout.addWidget(title)
-        layout.addWidget(self.value_label)
         layout.addWidget(graph_placeholder)
         layout.addStretch()
         
         self.setLayout(layout)
-
-    def update_value(self, temperature, humidity):
-        self.value_label.setText(
-            f"Latest Temperature: {temperature} C\nLatest Humidity: {humidity}%"
-        )
-
-# process sensor 3
-class Sensor3Page(QWidget):
+        
+class StepperControls(QWidget):
     def __init__(self):
         super().__init__()
         
         layout = QVBoxLayout()
         
         # Page title
-        title = QLabel("Light")
+        title = QLabel("Stepper Motor Controls")
         title.setAlignment(Qt.AlignmentFlag.AlignLeft)
         title.setStyleSheet("font-size: 20px;")
         
         # Graph placeholder
-        graph_placeholder = QLabel("JSkdfjskfjdsklfds")
+        graph_placeholder = QLabel("fhdsjkfhjdskhfjdsk")
         graph_placeholder.setAlignment(Qt.AlignmentFlag.AlignLeft)
         graph_placeholder.setStyleSheet("border: 2px dashed gray; background-color: #ffe6e6;")
         graph_placeholder.setMinimumSize(400, 250)
-
-        self.value_label = QLabel("Latest Light: --")
-        self.value_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
         
         layout.addWidget(title)
-        layout.addWidget(self.value_label)
         layout.addWidget(graph_placeholder)
         layout.addStretch()
         
         self.setLayout(layout)
 
-    def update_value(self, value):
-        self.value_label.setText(f"Latest Light: {value}")
         
 # design the main window
 class MainInterface(QMainWindow):
@@ -136,68 +111,49 @@ class MainInterface(QMainWindow):
         
         # global gui things
         self.setMinimumSize(QSize(800, 400))
-        self.setWindowTitle("Lab 01 - Sensors")
+        self.setWindowTitle("Lab 02 - Motors")
         
-        # init the sidebar, init pages for sensors 
-        self.sidebar = Sidebar()
-        self.s1_page = Sensor1Page()
-        self.s2_page = Sensor2Page()
-        self.s3_page = Sensor3Page()
-        
-        # stacked widget for multiple pages
-        self.stacked_widget = QStackedWidget()
-        self.stacked_widget.addWidget(self.s1_page)  # Index 0
-        self.stacked_widget.addWidget(self.s2_page)  # Index 1
-        self.stacked_widget.addWidget(self.s3_page)  # Index 2
+        # define widgets
+        self.rc_ctrl = RCMotorControls()
+        self.dc_ctrl = DCMotorControls()
+        self.stepper_ctrl = StepperControls()
         
         # use hbox to put sidebar next to panels
         central_widget = QWidget()
         main_layout = QHBoxLayout()
         
-        # add components to main layout
-        main_layout.addWidget(self.sidebar)
-        main_layout.addWidget(self.stacked_widget)
+        # set controllers
+        main_layout.addWidget(self.rc_ctrl)
+        main_layout.addWidget(self.dc_ctrl)
+        main_layout.addWidget(self.stepper_ctrl)
         
         # set central widget and layout
         central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
         
-        # toggle on button click
-        self.sidebar.s1_btn.clicked.connect(lambda: self.TogglePage(0, self.sidebar.s1_btn))
-        self.sidebar.s2_btn.clicked.connect(lambda: self.TogglePage(1, self.sidebar.s2_btn))
-        self.sidebar.s3_btn.clicked.connect(lambda: self.TogglePage(2, self.sidebar.s3_btn))
-        
-        # initial pg
-        self.TogglePage(0, self.sidebar.s1_btn)
-        
-        self.serial_timer = QTimer(self)
-        self.serial_timer.setInterval(50)
-        self.serial_timer.timeout.connect(self.poll_serial)
-        self.serial_timer.start()
-    
-    # toggle which sensor data is shown
-    def TogglePage(self, index, button):
-        self.stacked_widget.setCurrentIndex(index)
-        # self.sidebar.highlight_button(button) # TODO IF TIME
+        # self.serial_timer = QTimer(self)
+        # self.serial_timer.setInterval(50)
+        # self.serial_timer.timeout.connect(self.poll_serial)
+        # self.serial_timer.start()
 
-    def poll_serial(self):
-        try:
-            line = arduino.readline().decode("utf-8", errors="ignore").strip()
-        except serial.SerialException:
-            return
+    # def poll_serial(self):
+    #     try:
+    #         line = arduino.readline().decode("utf-8", errors="ignore").strip()
+    #     except serial.SerialException:
+    #         return
 
-        if not line:
-            return
+    #     if not line:
+    #         return
 
-        # arduino outputs (distance,humidity,temperature,light)
-        parts = [p.strip() for p in line.split(",")]
-        if len(parts) < 4:
-            return
+    #     # arduino outputs (distance,humidity,temperature,light)
+    #     parts = [p.strip() for p in line.split(",")]
+    #     if len(parts) < 4:
+    #         return
 
-        distance, humidity, temperature, light = parts[0], parts[1], parts[2], parts[3]
-        self.s1_page.update_value(distance)
-        self.s2_page.update_value(temperature, humidity)
-        self.s3_page.update_value(light)
+    #     distance, humidity, temperature, light = parts[0], parts[1], parts[2], parts[3]
+    #     self.s1_page.update_value(distance)
+    #     self.s2_page.update_value(temperature, humidity)
+    #     self.s3_page.update_value(light)
         
 
 # run the app
